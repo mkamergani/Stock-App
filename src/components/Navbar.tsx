b580378c52b7bf024f9c7336c09aaca57970424e
Mohamed Khalid <mkmergani@gmail.com>
2023-07-17 23:41:34 +0300

    feat: add navbar functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,50 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { ArrowSwapVertical, Activity, MessageText } from 'iconsax-react'
 
 function Navbar() {
+  const [active, setActive] = useState('markets')
+  const handleClick = (page: string) => {
+    return () => setActive(page)
+  }
   return (
-    <div className="flex items-center justify-center gap-x-10">
-      <button type="button">
-        <div className=" flex h-[100px] w-[100px] flex-col items-center justify-center gap-y-2 rounded-xl p-2 hover:bg-blue-100">
-          <Activity size={40} variant="Outline" />
-          <p className="text-base font-semibold">Portfolio</p>
-        </div>
+    <div className="flex items-center justify-center gap-x-10 pb-6 ">
+      <button type="button" onClick={handleClick('portfolio')}>
+        <Link to="/">
+          <div
+            className={` ${
+              active === 'portfolio' ? 'text-blue-600' : ''
+            } flex h-[100px] w-[100px] flex-col items-center justify-center gap-y-2 rounded-xl p-2 hover:bg-blue-100`}
+          >
+            <Activity size={40} variant="Outline" />
+            <p className="text-base font-semibold">Portfolio</p>
+          </div>
+        </Link>
       </button>
-      <button type="button">
-        <div className=" flex h-[100px] w-[100px] flex-col items-center justify-center gap-y-2 rounded-xl p-2 hover:bg-blue-100">
-          <ArrowSwapVertical size={40} variant="Outline" />
-          <p className="text-base font-semibold">Markets</p>
-        </div>
+      <button type="button" onClick={handleClick('markets')}>
+        <Link to="/">
+          <div
+            className={` ${
+              active === 'markets' ? 'text-blue-600' : ''
+            } flex h-[100px] w-[100px] flex-col items-center justify-center gap-y-2 rounded-xl p-2 hover:bg-blue-100`}
+          >
+            <ArrowSwapVertical size={40} variant="Outline" />
+            <p className="text-base font-semibold">Markets</p>
+          </div>
+        </Link>
       </button>
-      <button type="button">
-        <div className=" flex h-[100px] w-[100px] flex-col items-center justify-center gap-y-2 rounded-xl p-2 hover:bg-blue-100">
-          <MessageText size={40} variant="Outline" />
-          <p className="text-base font-semibold">News</p>
-        </div>
+
+      <button type="button" onClick={handleClick('news')}>
+        <Link to="/">
+          <div
+            className={` ${
+              active === 'news' ? 'text-blue-600' : ''
+            } flex h-[100px] w-[100px] flex-col items-center justify-center gap-y-2 rounded-xl p-2 hover:bg-blue-100`}
+          >
+            <MessageText size={40} variant="Outline" />
+            <p className="text-base font-semibold">News</p>
+          </div>
+        </Link>
       </button>
     </div>
   )
